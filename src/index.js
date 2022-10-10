f18c6b5659b5b5532df266e53e72103877248720
Mohd Faizan <gitfaizan0496@gmail.com>
2022-10-10 12:52:06 +0530

    add redux in app and use reducers as well as action

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import  {createStore}  from 'redux';
+import movies from './reducer/index';
+
+
+const store = createStore(movies);
+console.log('store ',store);
+ console.log('Before  state ',store.getState());
+ store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{name:'superman'}]
+ })
+ console.log('after state',store.getState());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
